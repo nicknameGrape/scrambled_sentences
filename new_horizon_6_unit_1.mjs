694bed97caae2ac72f89af0fa8ed547ce1075db2
GR <kurumeteacher@gmail.com>
2025-04-22 22:37:26 +0900

    added new horizon 6 unit 1 chant and song

---
+++ new_horizon_6_unit_1.mjs
@@ -0,0 +1,41 @@
+export const NH6_U1 = {
+	"title": "Unit 1: This is me!",
+	"startPage": 6,
+	"sections": [
+		{
+			"title": "Let's Chant: My treasure is this ball.",
+			"sentences": [
+				{"page": 6, "speaker": "ゴリラのビリー", "english": "Hi, I'm Billy.", "japanese": "こんにちは、ビリーです。"},
+				{"page": 6, "speaker": "ゴリラのビリー", "english": "I'm from Mie.", "japanese": "三重県出身です。"},
+				{"page": 6, "speaker": "ゴリラのビリー", "english": "I like baseball.", "japanese": "野球が好きです。"},
+				{"page": 6, "speaker": "ゴリラのビリー", "english": "My treasure is this_ball.", "japanese": "私の宝物はこのボールです。"},
+				{"page": 6, "speaker": "ゴリラのビリー", "english": "It's from my friend.", "japanese": "友達からもらったものです。"},
+				{"page": 6, "speaker": "きつねのペトラ", "english": "Hi, I'm Petra.", "japanese": "こんにちは、ペトラです。"},
+				{"page": 6, "speaker": "きつねのペトラ", "english": "I'm from Russia.", "japanese": "ロシア出身です。"},
+				{"page": 6, "speaker": "きつねのペトラ", "english": "I like cooking.", "japanese": "料理が好きです。"},
+				{"page": 6, "speaker": "きつねのペトラ", "english": "My treasure is this_pan.", "japanese": "私の宝物はこのフライパンです。"},
+				{"page": 6, "speaker": "きつねのペトラ", "english": "It's from my friend.", "japanese": "友達からもらったものです。"},
+				{"page": 6, "speaker": "みんな", "english": "What's your name?", "japanese": "あなたの名前は？"},
+				{"page": 6, "speaker": "みんな", "english": "What's your treasure?", "japanese": "あなたの宝物は何ですか？"}
+			]
+		},
+		{
+			"title": "Let's Sing: Hello, everyone.",
+			"sentences": [
+				{"page": 6, "speaker": "さき", "english": "Hello, everyone.", "japanese": "みなさん、こんにちは。"},
+				{"page": 6, "speaker": "さき", "english": "<em>Konnichiwa</em>, I'm Saki.", "japanese": "こんにちは、サキです。"},
+				{"page": 6, "speaker": "さき", "english": "I'm from Japan.", "japanese": "日本から来ました。"},
+				{"page": 6, "speaker": "さき", "english": "I like music and English.", "japanese": "音楽と英語が好きです。"},
+				{"page": 6, "speaker": "さき", "english": "It's nice to_meet you_all.", "japanese": "みなさん、はじめまして。"},
+				{"page": 6, "speaker": "ルカス", "english": "Olá, I'm Lucas.", "japanese": "オラ、ルーカスです。"},
+				{"page": 6, "speaker": "ルカス", "english": "I'm from Brazil.", "japanese": "ブラジルから来ました。"},
+				{"page": 6, "speaker": "ルカス", "english": "I'm good_at playing soccer.", "japanese": "サッカーが得意です。"},
+				{"page": 6, "speaker": "ナディア", "english": "Jambo, I'm Nadia.", "japanese": "ジャンボ、ナディアです。"},
+				{"page": 6, "speaker": "ナディア", "english": "I'm from Kenya.", "japanese": "ケニアから来ました。"},
+				{"page": 6, "speaker": "ナディア", "english": "I have a_dog.", "japanese": "犬を飼っています。"},
+				{"page": 6, "speaker": "ナディア", "english": "I like animals.", "japanese": "動物が好きです。"},
+				{"page": 6, "speaker": "みんな", "english": "It's nice to_meet you_all.", "japanese": "みなさん、はじめまして。"}
+			]
+		}
+	]
+};
--- textbooks.mjs
+++ textbooks.mjs
@@ -1,7 +1,14 @@
+import {NH6_U1} from "./new_horizon_6_unit_1.mjs";
 import {JS2_P1} from "./junior_sunshine_2_program_1.mjs";
 import {JS2_P4} from "./junior_sunshine_2_program_4.mjs";
 
 export const BOOKS = {
+	"New Horizon 6": {
+		"title": "New Horizon 6",
+		"contents": [
+			NH6_U1
+		]
+	},
 	"Sunshine 2": {
 		"title": "Sunshine 2",
 		"contents": [
