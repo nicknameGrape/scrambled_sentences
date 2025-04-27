571178fb0178e6596201e5f941ca2fc034373144
GR <kurumeteacher@gmail.com>
2025-04-27 22:02:02 +0900

    added new_horizon_g5_unit_1_lets_chant

---
+++ new_horizon_5_unit_1.mjs
@@ -0,0 +1,29 @@
+export const NH5_U1 = {
+	"title": "Unit 1: Hello, Friends!",
+	"startPage": 8,
+	"sections": [
+		{
+			"title": "Let's Chant: How do you spell your name?",
+			"sentences": [
+				{"page": 8, "speaker": "みんな", "english": "How do_you spell your name?", "japanese": "名前の綴りは？"},
+				{"page": 8, "speaker": "ごりらのオスカー", "english": "My name is Oscar.", "japanese": "私の名前はオスカーです。"},
+				{"page": 8, "speaker": "ごりらのオスカー", "english": "O S C A R.", "japanese": "O-S-C-A-R."},
+				{"page": 8, "speaker": "みんな", "english": "How do_you spell your name?", "japanese": "名前の綴りは？"},
+				{"page": 8, "speaker": "トカゲのガブリエル", "english": "My name is Gabriel.", "japanese": "私の名前はガブリエルです。"},
+				{"page": 8, "speaker": "トカゲのガブリエル", "english": "G A B R I E L.", "japanese": "G-A-B-R-I-E-L."},
+				{"page": 8, "speaker": "ごりらのオスカー", "english": "Nice to_meet you.", "japanese": "初めまして。"},
+				{"page": 8, "speaker": "トカゲのガブリエル", "english": "Nice to_meet you, too!", "japanese": "こちらこそ、初めまして！"}
+			]
+		},
+		{
+			"title": "Let's Chant: What subject dojyou like?",
+			"sentences": [
+				{"page": 8, "speaker": "みんな", "english": "What subject do_you like?", "japanese": "好きな教科は何ですか？"},
+				{"page": 8, "speaker": "ごりらのオスカー", "english": "I like music.", "japanese": "音楽が好きです。"},
+				{"page": 8, "speaker": "きつねのペトラ", "english": "I like science.", "japanese": "理科が好きです。"},
+				{"page": 8, "speaker": "トカゲのガブリエル", "english": "I like home_economics.", "japanese": "家庭科が好きです。"},
+				{"page": 8, "speaker": "みんな", "english": "I like English.", "japanese": "英語が好きです。"}
+]
+		}
+	]
+};
--- textbooks.mjs
+++ textbooks.mjs
@@ -1,8 +1,15 @@
+import {NH5_U1} from "./new_horizon_5_unit_1.mjs";
 import {NH6_U1} from "./new_horizon_6_unit_1.mjs";
 import {JS2_P1} from "./junior_sunshine_2_program_1.mjs";
 import {JS2_P4} from "./junior_sunshine_2_program_4.mjs";
 
 export const BOOKS = {
+	"New Horizon 5": {
+		"title": "New Horizon 5",
+		"contents": [
+			NH5_U1
+		]
+	},
 	"New Horizon 6": {
 		"title": "New Horizon 6",
 		"contents": [
