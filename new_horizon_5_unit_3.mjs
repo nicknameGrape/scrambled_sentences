854676ff915aee5e143260baeeb2258dd34c48cc
GR <kurumeteacher@gmail.com>
2025-06-15 20:49:32 +0900

    add song and chants from new_horizon_5_unit_3

---
+++ new_horizon_5_unit_3.mjs
@@ -0,0 +1,49 @@
+export const NH5_U3 = {
+	"title": "Unit 3: Can you play dodgeball?",
+	"startPage": 28,
+	"sections": [
+		{
+			"title": "Let's Chant: I can dance well.",
+			"sentences": [
+				{"page": "28", "speaker": "とかげのガブリエル", "english": "Hi, I'm Gabriel.", "japanese": "こんにちは、ガブリエルです。"},
+				{"page": "28", "speaker": "とかげのガブリエル", "english": "I like dancing.", "japanese": "ダンスが好きです。"},
+				{"page": "28", "speaker": "とかげのガブリエル", "english": "I can dance well.", "japanese": "ダンスが上手です。"},
+				{"page": "28", "speaker": "ゴリラのオスカー", "english": "Hi, I'm Oscar.", "japanese": "こんにちは、オスカーです。"},
+				{"page": "28", "speaker": "ゴリラのオスカー", "english": "I like music.", "japanese": "音楽が好きです。"},
+				{"page": "28", "speaker": "ゴリラのオスカー", "english": "I can play the_drums.", "japanese": "ドラムを演奏できます。"},
+				{"page": "28", "speaker": "みんな", "english": "How about you?", "japanese": "あなたはどうですか？"}
+			]
+		},
+		{
+			"title": "Let's Chant: Can you swim?",
+			"sentences": [
+				{"page": "28", "speaker": "ペンギン", "english": "I can swim.", "japanese": "私は泳げます。"},
+				{"page": "28", "speaker": "ペンギン", "english": "Can you swim?", "japanese": "あなたは泳げますか？"},
+				{"page": "28", "speaker": "トカゲのヨナ", "english": "Yes, I can.", "japanese": "はい、泳げます。"},
+				{"page": "28", "speaker": "ペンギン", "english": "I can cook.", "japanese": "私は料理ができます。"},
+				{"page": "28", "speaker": "ペンギン", "english": "Can you cook?", "japanese": "あなたは料理できますか？"},
+				{"page": "28", "speaker": "ゴリラのビリー", "english": "Yes, I can.", "japanese": "はい、泳げます。"},
+				{"page": "28", "speaker": "ペンギン", "english": "I can't fly well.", "japanese": "私は飛ぶのが得意ではありません。"},
+				{"page": "28", "speaker": "ペンギン", "english": "Can you fly well?", "japanese": "あなたは上手に飛べますか？"},
+				{"page": "28", "speaker": "キツネのベラ", "english": "No, I can't.", "japanese": "いいえ、飛べません。"},
+				{"page": "28", "speaker": "ペンギン", "english": "Can you speak English?", "japanese": "あなたは英語を話せますか？"},
+				{"page": "28", "speaker": "みんな", "english": "Yes, we can!", "japanese": "はい、話せます！"}
+			]
+		},
+		{
+			"title": "Let's Sing: Dodge the ball.",
+			"sentences": [
+				{"page": "28", "speaker": "だいち", "english": "You can throw the_ball.", "japanese": "ボールを投げることができます。"},
+				{"page": "28", "speaker": "だいち", "english": "I can catch the_ball.", "japanese": "私はボールをキャッチできます。"},
+				{"page": "28", "speaker": "さき", "english": "You can run.", "japanese": "あなたは走ることができます。"},
+				{"page": "28", "speaker": "さき", "english": "You can stop.", "japanese": "あなたは止まることができます。"},
+				{"page": "28", "speaker": "さき", "english": "You can go.", "japanese": "あなたは進むことができます。"},
+				{"page": "28", "speaker": "さき", "english": "You can dodge the_ball.", "japanese": "あなたはボールを避けることができます。"},
+				{"page": "28", "speaker": "さき", "english": "Come_on!", "japanese": "がんばれ!"},
+				{"page": "28", "speaker": "だいち", "english": "You can't dodge the_ball.", "japanese": "あなたはボールを避けることはできません。"},
+				{"page": "28", "speaker": "さき", "english": "No!  You can't kick the_ball.", "japanese": "だめです！あなたはボールを蹴ることはできません。"},
+				{"page": "28", "speaker": "だいち", "english": "We win!", "japanese": "勝ったよ！"}
+			]
+		}
+	]
+};
--- textbooks.mjs
+++ textbooks.mjs
@@ -1,5 +1,6 @@
 import {NH5_U1} from "./new_horizon_5_unit_1.mjs";
 import {NH5_U2} from "./new_horizon_5_unit_2.mjs";
+import {NH5_U3} from "./new_horizon_5_unit_3.mjs";
 import {NH6_U1} from "./new_horizon_6_unit_1.mjs";
 import {JS2_P1} from "./junior_sunshine_2_program_1.mjs";
 import {JS2_P4} from "./junior_sunshine_2_program_4.mjs";
@@ -9,7 +10,8 @@ export const BOOKS = {
 		"title": "New Horizon 5",
 		"contents": [
 			NH5_U1,
-			NH5_U2
+			NH5_U2,
+			NH5_U3
 		]
 	},
 	"New Horizon 6": {
